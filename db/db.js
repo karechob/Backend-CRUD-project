9f368c8408cb6b0b9834320a6d74afe6e574be30
AliAngelinaaa <nanzum1140@gmail.com>
2023-06-29 14:50:51 -0400

    user

--- db/db.js
+++ db/db.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require("sequelize");
 const { name } = require("../package.json");
 
-const db = new Sequelize(`postgres://localhost:5432/${name}`, {
-    logging: false,
-});
+// const db = new Sequelize(`postgres://localhost:5432/${name}`, {
+//     logging: false,
+// });
+
+const db = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  });
 
 module.exports = db;
