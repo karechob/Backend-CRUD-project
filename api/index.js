a135a830f8ce1f06e47cc449d504210de4cf3be1
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-30 14:59:58 -0400

    Added commentation for all api files

--- api/index.js
+++ api/index.js
@@ -1,15 +1,24 @@
-const router = require("express").Router();
+const router = require("express").Router();//Creates a router object
 
 // Already mounted on /api/
 router.use("/allcampuses", require("./Allcampus"));
+//Gets and Mounts the "/allcampuses" route
+
 router.use("/allstudents", require("./Allstudents"));
+//Gets and Mounts the "/allstudents" route
+
 router.use("/campus",require("./campus"));
+//Gets and Mounts the "/campus" route
+
 router.use("/student",require("./student"));
-// 404 Handling
+// Gets and Mounts the "/student" route
+
+//Error Handling for error 404 Handling
 router.use((req, res, next) => {
   const error = new Error("404 Not Found");
   error.status = 404;
   next(error);
 });
 
+// Export the router object
 module.exports = router;
