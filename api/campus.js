b46a7ec5a4fa659301026a5da18a072b68c53472
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-30 14:39:37 -0400

    Commented the file campus

--- api/campus.js
+++ api/campus.js
@@ -1,46 +1,58 @@
-const express = require('express');
-const router = express.Router();
-const { Campus, Student } = require('../db/models');
+const express = require('express'); //Imports the Express module
+const router = express.Router();//Creates a router object 
+const { Campus, Student } = require('../db/models'); //Imports the Campus and Student models
 
 router.get('/:id', async (req, res, next) => {
+  // Retrieving a specific campus by ID
   try {
     const campusId = req.params.id;
     const campus = await Campus.findByPk(campusId, { include: Student });
 
     if (!campus) {
+  // If the campus is not found, send a response with status code 404 
+  //And the error message (Campus not found)
       return res.status(404).json({ error: 'Campus not found' });
     }
-
+  // Sending a response with the retrieved campus
     res.json(campus);
   } catch (error) {
+// Pass any error to the error handling
     next(error);
   }
 });
 
+//Creation of a new campus
 router.post('/', async (req, res, next) => {
   try {
       const { name, imageUrl, address, description } = req.body;
 
+// Creating a new campus with the provided data
       const newCampus = await Campus.create({ name, imageUrl, address, description });
 
       res.status(201).json(newCampus);
+// Send a response with status code 201 and the newly created campus
   } catch (error) {
-
+//Handling any errors that occur
       next(error);
   }
 });
 
+//Deletion of a Campus
 router.delete('/:id', async (req, res, next) => {
   try {
     const campusId = req.params.id;
 
+// Delete the campus with the provided ID from the database
     await Campus.destroy({ where: { id: campusId } });
 
     res.json({ message: 'Campus removed successfully' });
+//Send response message (Campus removed successfully)
   } catch (error) {
 
+//Handling any errors that occur
     next(error);
   }
 });
 
+//Exports the router object.
 module.exports = router;
