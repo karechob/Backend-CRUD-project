bd3d61c160c8628998678bdd767c7fcddc54f373
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-30 12:30:06 -0400

    Commented out the parts of removeStudent file

--- api/removeStudent.js
+++ api/removeStudent.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const { Student } = require('../db/models');
+const express = require('express'); //Imports the Express module
+const router = express.Router(); //Creates a router object
+const { Student } = require('../db/models'); //Imports the Student model 
 
-
+// Route handler for DELETE request
 router.delete('/student/:id', async (req, res, next) => {
-    try {
+    try { // Extract the studentId from the request parameters
         const studentId = req.params.id;
 
+        // Delete the student with the specified id from the database
         await Student.destroy({ where: { id: studentId } });
 
+        // Send a response  saying (Student removed successfully), taske completed
         res.json({ message: 'Student removed successfully' });
     } catch (error) {
+        //Error handling
         next(error);
     }
 });
-
+// Export the router object
 module.exports = router;
