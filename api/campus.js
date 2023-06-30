9fec5f613d6442c2ac9e5eb5ae3f4001bb26aa06
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-30 12:17:33 -0400

    commented the functions of the campus file

--- api/campus.js
+++ api/campus.js
@@ -1,19 +1,26 @@
-const express = require("express");
-const router = express.Router();
-const { Campus } = require("../db/models");
+const express = require("express"); //Imports the Express module
+const router = express.Router(); //Creates a router object
+const { Campus } = require("../db/models"); // Imports the Campus model
 
 // Root here is localhost:8080/api/Campuss/
 
+// Route handler for GET request
 router.get("/", async (req, res, next) => {
-  try {
+  try { // Retrieve all campuses from the database
     const allCampus = await Campus.findAll();
     console.log("this is all campus: " + allCampus)
+    
+// If campuses are found, send a response with status code 200 and the campuses serialized as JSON
+// If no campuses are found, send a response with status code 404 and a plain text message
+  
     allCampus
       ? res.status(200).json(allCampus)
       : res.status(404).send("Campus List Not Found");
+
   } catch (error) {
-    console.log(error);
+    console.log(error);  // Log any errors that occur during the execution
   }
 });
 
+// Export the router object
 module.exports = router;
