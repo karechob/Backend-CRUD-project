f2ac6e063250d79c9140a74fea78714ce81cfa17
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-30 12:39:30 -0400

    Commented single student file

--- api/singleStudent.js
+++ api/singleStudent.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const { Student, Campus } = require('../db/models');
+const express = require('express'); // Imports the Express module
+const router = express.Router(); //Creates a router object
+const { Student, Campus } = require('../db/models'); // Imports the Student and Campus models
 
+// Route handler for GET request
 router.get('/student/:id', async (req, res, next) => {
-    try {
+    try {// Extract the studentId from the request parameters
         const studentId = req.params.id;
 
+        // Find the student with the specified id, including associated campus
         const student = await Student.findByPk(studentId, { include: Campus });
 
         if (!student) {
+     // If student is not found, send a response with status code 404 and message (Student not found)
             return res.status(404).json({ error: 'Student not found' });
         }
-
+// Send a response with the found student, including associated campus
         res.json(student);
     } catch (error) {
+    //Handles any errors that occur
         next(error);
     }
 });
 
+// Export the router object
 module.exports = router;
