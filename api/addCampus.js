5809703f242aba77b2e18f3daee5a87a101b5b53
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-30 11:55:03 -0400

    commented out all parts of addCampus

--- api/addCampus.js
+++ api/addCampus.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const { Campus, Student } = require('../db/models');
+const express = require('express'); // Imports the Express module
+const router = express.Router(); //Creates a router object 
+const { Campus, Student } = require('../db/models'); //Imports the Campus and Student models from the folders
 
+//Defines a route handler for the POST request
 router.post('/campus', async (req, res, next) => {
-    try {
+    try { //Destructures the below shown properties
         const { name, imageUrl, address, description } = req.body;
-
+    //creates a new campus object from the campus model
         const newCampus = await Campus.create({ name, imageUrl, address, description });
 
         res.status(201).json(newCampus);
     } catch (error) {
-
+        //Handles errors that occurs
         next(error);
     }
 });
 
-module.exports = router;
+//Exports the router object to make it available 
+
+module.exports = router; 
